722d5ca86030fdc37950a10c524fcd2eec1f1bbe
nikitafreez <nikita2002393@gmail.com>
2022-01-25 19:36:10 +0300

    Коммит в VS

---
+++ gitConsoleApp/gitConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gitConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //Session_vs
+        }
+    }
+}
